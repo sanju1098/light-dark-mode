bc384b3dce04286100c4f2bcc54933368f6db15b
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2024-03-14 04:52:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ThemeProvider } from "./theme-context";
+
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+
+import "./styles.css";
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
